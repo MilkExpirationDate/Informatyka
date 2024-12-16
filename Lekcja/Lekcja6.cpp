53f9bd74fb97de055e4451abb0136fe687828775
bkawecki691 <eudeak120@gmail.com>
2024-12-16 15:03:33 +0100

    Komentarz

---
+++ Lekcja/Lekcja6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int N;
+cout << "Podaj liczbe N: " ;
+cin >> N ;
+
+int wynik = 0;
+
+for ( int i = 1; i <= N; i++)
+{
+	wynik = wynik + i ;
+}
+
+cout << "Suma liczb od 1 do " << N << ": " << wynik << endl;
+
+
+
+
+}
